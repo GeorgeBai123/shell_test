ce8ed08e52a21863ae92d206b8eebf2bddd17ba1
GeorgeBai123 <xiao12312315@sina.com>
2017-06-15 18:43:01 +0800

    6/15 update

--- base/condition.sh
+++ base/condition.sh
@@ -1,18 +1,17 @@
 #/bin/sh
 #
-#shell中对变量的值添加单引号，双引号和不添加的区别：对类型来说是无关的，即不是添加了引号就变成了字符串类型，
-#单引号不对相关量进行替换，如不对$符号解释成变量引用，从而用对应变量的值替代，双引号则会进行替代
+#
+#双引号解析变量,单引号不解析变量
 
 A="$1"
 B="$2"
 	echo "输入的原始值：A=$A,B=$B"
 
-#判断字符串是否相等
+#######################################判断字符串是否相等###########################################
 if [ "$A" = "$B" ];then
 	echo "[$A = $B]"
 fi
-exit;
-#判断字符串是否相等，与上面的=等价
+
 if [ "$A" == "$B" ];then
 	echo "[ $A == $B]"
 fi
@@ -29,56 +28,118 @@ if [[ "$A" == "a*" ]];then
 	echo "==/"a*/""
 fi
 
-
 #File globbing(通配) 和word splitting将会发生, 此时的a*会自动匹配到对应的当前以a开头的文件
 #如在当前的目录中有个文件：add_crontab.sh,则下面会输出ok
 #if [ "add_crontab.sh" == a* ];then
 #	echo "ok"
 #fi
 if [ "$A" == a* ];then
-	echo "[ ==a* ]"
+	echo "[ $A==a* ]"
 fi
 
 #如果$a等于a*(字符匹配),那么结果为true
 if [ "$A" == "a*" ];then
-	echo "==/"a*/""
+	echo "$A==/"a*/""
 fi
 
 #字符串不相等
 if [ "$A" != "$B" ];then
-	echo "[ != ]"
+	echo "[ $A!= $B]"
 fi
 
 #字符串不相等
 if [[ "$A" != "$B" ]];then
-	echo "[[ != ]]"
+	echo "[[ $A!= $B]]"
 fi
 
 #字符串不为空，长度不为0
 if [ -n "$A" ];then
-	echo "[ -n ]"
+	echo "[ $A not null]"
 fi
 
 #字符串为空.就是长度为0.
 if [ -z "$A" ];then
-	echo "[ -z ]"
+	echo "[  $A is null]"
 fi
 
 #需要转义<，否则认为是一个重定向符号
 if [ $A < $B ];then
-	echo "[ < ]"
+	echo "[ $A< $B]"
 fi
 
 if [[ $A < $B ]];then
-	echo "[[ < ]]"
+	echo "[[  $A<$B ]]"
 fi
 
 #需要转义>，否则认为是一个重定向符号
-if [ $A > $B ];then
-	echo "[ > ]"
+if [ $A \> $B ];then
+	echo "[  $A>$B ]"
 fi
 
 if [[ $A > $B ]];then
-	echo "[[ > ]]"
+	echo "[[  $A> $B]]"
 fi
 
+#######################################判断数字是否相等###########################################
+
+if [[ $A -eq $B ]];then
+	echo "$A=$B"
+fi
+
+if [[ $A -ne $B ]];then
+	echo "$A!=$B"
+fi
+
+if [[ $A -gt $B ]];then
+	echo "$A>$B"
+fi
+
+if [[ $A -ge $B ]];then
+	echo "$A>=$B"
+fi
+
+if [[ $A -le $B ]];then
+	echo "$A<=$B"
+fi
+
+if [[ $A -lt $B ]];then
+	echo "$A<$B"
+fi
+
+#######################################判断文件[]等价[[]]##########################################
+
+if [[ -f $A ]]; then
+	echo "$A is a normal file"
+fi
+
+if [[ -d $A ]]; then
+	echo "$A is a directory file"
+fi
+
+if [[ -e $A ]]; then
+	echo "$A is a exist file"
+fi
+
+if [[ -s $A ]]; then
+	echo "$A is a exist and not null file"
+fi
+
+if [[ -L $A ]]; then
+	echo "$A is a link file"
+fi
+
+if [[ -r $A ]]; then
+	echo "$A is a readable file"
+fi
+
+if [[ -w $A ]]; then
+	echo "$A is a writeable file"
+fi
+
+if [[ -x $A ]]; then
+	echo "$A is a executable file"
+fi
+
+if [[ $A -nt $B ]]; then
+	echo "$A is new than $B"
+fi
